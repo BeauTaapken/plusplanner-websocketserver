1c98bb6da285ea3df34135b9cc12fbf49b48c20d
Niek125 <niek.sleddens@gmail.com>
2019-12-14 13:47:58 +0100

    fixed message controller

--- src/main/java/plusplanner/websocketserver/controllers/MessageController.java
+++ src/main/java/plusplanner/websocketserver/controllers/MessageController.java
@@ -1,43 +1,41 @@
 package plusplanner.websocketserver.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
 import plusplanner.websocketserver.models.MessageTask;
-import plusplanner.websocketserver.models.Message;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class MessageController {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public MessageController() {
         mapper = new ObjectMapper();
     }
 
-    URL url = null;
-    URLConnection conn = null;
-    public void saveMessage(String json){
+    public void saveMessage(String json) {
         try {
             MessageTask m = mapper.readValue(json, MessageTask.class);
             String s = mapper.writeValueAsString(m.getElement());
-            url = new URL("http://localhost:8084/message/create/" + s);
-        } catch (MalformedURLException | JsonProcessingException e) {
+            restTemplate.postForObject("http://plus-planner-channel-service/message/create/" + s, new HttpEntity<>(new HttpHeaders()), String.class);
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
     }
 
-    public void deleteMessage(String json){
+    public void deleteMessage(String json) {
         try {
             MessageTask m = mapper.readValue(json, MessageTask.class);
-            url = new URL("http://localhost:8084/message/delete/" + m.getElement().getMessageid().toString());
-        } catch (MalformedURLException | JsonProcessingException e) {
+            restTemplate.postForObject("http://localhost:8084/message/delete/" + m.getElement().getMessageid(), new HttpEntity<String>(new HttpHeaders()), String.class);
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
     }
