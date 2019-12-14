a6eb332c55e908b4484964e0b3f5a75c0764e0f7
Niek125 <niek.sleddens@gmail.com>
2019-12-14 13:57:33 +0100

    subpartcontroller beans

--- src/main/java/plusplanner/websocketserver/controllers/SubPartController.java
+++ src/main/java/plusplanner/websocketserver/controllers/SubPartController.java
@@ -1,32 +1,32 @@
 package plusplanner.websocketserver.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import plusplanner.websocketserver.models.MessageTask;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
 import plusplanner.websocketserver.models.SubPartTask;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class SubPartController {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public SubPartController() {
         mapper = new ObjectMapper();
     }
-
-    URL url = null;
-    URLConnection conn = null;
     public void saveSubPart(String json){
         try {
             SubPartTask p = mapper.readValue(json, SubPartTask.class);
             String s = mapper.writeValueAsString(p.getElement());
-            url = new URL("http://localhost:8081/subpart/create/" + s);
-        } catch (MalformedURLException | JsonProcessingException e) {
+            restTemplate.postForObject("http://localhost:8081/subpart/create/" + s, new HttpEntity<>(new HttpHeaders()), String.class);
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
     }
@@ -34,8 +34,8 @@ public class SubPartController {
     public void deleteSubPart(String json){
         try {
             SubPartTask p = mapper.readValue(json, SubPartTask.class);
-            url = new URL("http://localhost:8081/subpart/delete/" + p.getElement().getPartid());
-        } catch (MalformedURLException | JsonProcessingException e) {
+            restTemplate.postForObject("http://localhost:8081/subpart/delete/" + p.getElement().getPartid(), new HttpEntity<>(new HttpHeaders()), String.class);
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
     }
@@ -44,8 +44,8 @@ public class SubPartController {
         try {
             SubPartTask p = mapper.readValue(json, SubPartTask.class);
             String s = mapper.writeValueAsString(p.getElement());
-            url = new URL("http://localhost:8081/subpart/update/" + s);
-        } catch (MalformedURLException | JsonProcessingException e) {
+            restTemplate.postForObject("http://localhost:8081/subpart/update/" + s, new HttpEntity<>(new HttpHeaders()), String.class);
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
     }
